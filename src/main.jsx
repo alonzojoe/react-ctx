a0a87bac819c81ec5cc1a4032efe892572455773
Joenell Canlas Alonzo <medusazxc123@gmail.com>
2024-08-11 22:20:25 +0800

    feat: setup starting project

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,22 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import { createRoot } from "react-dom/client";
+import { Provider } from "react-redux";
+import { combineReducers, createStore } from "redux";
+import { BrowserRouter } from "react-router-dom";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+import App from "./App.jsx";
+import productReducer from "./store/reducers/products.js";
+import "./index.css";
+
+const rootReducer = combineReducers({
+  shop: productReducer,
+});
+
+const store = createStore(rootReducer);
+
+createRoot(document.getElementById("root")).render(
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>
+);
